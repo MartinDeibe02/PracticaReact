0f68273cccc19738d98d1037b013e28381c02e6c
Martin <martindeibe@gmail.com>
2023-03-01 10:41:53 +0100

    Añadida proyecto spring para back

---
+++ back-spring/nba_react/src/main/java/com/martin/nba_react/nba_react/NbaReactApplication.java
@@ -0,0 +1,13 @@
+package com.martin.nba_react.nba_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaReactApplication.class, args);
+	}
+
+}
---
+++ back-spring/nba_react/src/test/java/com/martin/nba_react/nba_react/NbaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.nba_react.nba_react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
