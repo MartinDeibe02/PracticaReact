e192059f11673fc1e6ff28db8316f77b87f943ac
Martin <martindeibe@gmail.com>
2023-03-01 20:31:23 +0100

    Cambios en navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import '../css/Home.css'
 import { Footer } from './Footer'
 import { Navbar } from './Navbar'
 import { Products } from './Products'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Home = () => {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import logo from '../images/logo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
+import "bootstrap/js/src/collapse.js";
+
+
 
 export const Navbar = () => {
   return (
-    <div className='navBar'>
-        <div className='img'>
-            <img src={logo} alt='logo'></img>
-        </div>
-        <div className='derecha'>
-            <Link to='/login' className='link'>login</Link>
-            <Link to='/signup' className='link'>signup</Link>
-        </div>
+    <div class="hero">
+
+        <nav class="navbar navbar-light bg-transparent navbar-expand-md bg-faded justify-content-center">
+            <div class="navbar-brand d-flex w-50 mr-auto logo">NBA <span className="logo2">Store</span> </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+                
+                <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+                    <li class="nav-item">
+                        <button className="button"><Link to='/login' className='link'>Login</Link></button>
+                    </li>
+                    <li class="nav-item">
+                        <button className="button"><Link to='/signup' className='link'>Signup</Link></button>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+
     </div>
-  )
-}
+
+
+  );
+};
