d67ced9f10f133777e47be39a932da0dbcb2bcb1
Martin <martindeibe@gmail.com>
2023-03-02 19:08:59 +0100

    Terminado añadir jersey

--- src/components/AddJersey.js
+++ src/components/AddJersey.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-
+import {storage, db}  from '../config/config'
 
 
 
@@ -11,27 +11,69 @@ export const AddJersey = () => {
     const [jerseyImage, setJerseyImage] = useState(null);
     const [error, setError] = useState('');
 
+    const types = ['image/png', 'image/jpeg']
+
+    const jerseyImgHandler = (e) => {
+      let archivo = e.target.files[0];
+      if(archivo && types.includes(archivo.type)){
+        setJerseyImage(archivo);
+        setError('');
+      }else{
+        setJerseyImage(null);
+        setError('Invalid image');
+      }
+    }
+
+    const addJersey = (e) =>{
+      e.preventDefault();
+      const upload = storage.ref(`imagenes-nba/${jerseyImage.name}`).put(jerseyImage);
+      upload.on('state_changed', snapshot=>{
+        const progreso = (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+        console.log(progreso);
+      },err=>{
+        setError(err.message)
+    },()=>{
+      
+      storage.ref('imagenes-nba').child(jerseyImage.name).getDownloadURL().then(url=>{
+        db.collection('Productos').add({
+          JerseyName: jerseyName,
+          JerseyDescription: jerseyDescription,
+          JerseyPrice: Number(jerseyPrice),
+          JerseyImage: url
+
+        }).then(()=>{
+          setJerseyName('');
+          setJerseyDescription('');
+          setJerseyPrice(0);
+          setJerseyImage('');
+          setError('');
+          document.getElementById('file').value = '';
+        }).catch(err=>setError(err.message));
+      })
+    })
+    }
+
   return (
     <div className='container'>
         <br/>
             <h2>ADD JERSEY</h2>
         <hr/>
-        <form autoComplete='off' className='form-group'>
+        <form autoComplete='off' className='form-group' onSubmit={addJersey}>
             <label htmlFor='jersey-name'>Jersey Name</label>
             <br/>
-            <input type='text' className='form-control' required
+            <input type='text' className='form-control' required onChange={(e) => setJerseyName(e.target.value)} value={jerseyName}
             />
             <label htmlFor='jersey-description'>Description</label>
             <br/>
-            <input type='text' className='form-control' required
+            <input type='text' className='form-control' required onChange={(e) => setJerseyDescription(e.target.value)} value={jerseyDescription}
             />
             <label htmlFor='jersey-price'>Jersey Price</label>
             <br/>
-            <input type='number' className='form-control' required
+            <input type='number' className='form-control' required onChange={(e) => setJerseyPrice(e.target.value)} value={jerseyPrice}
             />
             <label htmlFor='jersey-image'>Jersey Image</label>
             <br/>
-            <input type='file' className='form-control'
+            <input type='file' className='form-control' onChange={jerseyImgHandler} id='file'
             />
             <br/>
             <button className='btn btn-success btn-md'>ADD</button>
--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,23 @@
-import * as firebase from 'firebase';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyAwMQyGcoXeMyJMMFzpxvPKy3VZ434JskM",
     authDomain: "nba-react-c2fef.firebaseapp.com",
+    databaseURL: "https://nba-react-c2fef-default-rtdb.firebaseio.com",
     projectId: "nba-react-c2fef",
     storageBucket: "nba-react-c2fef.appspot.com",
     messagingSenderId: "716045653724",
     appId: "1:716045653724:web:9d54e19bc5d7b0ab13c619",
     measurementId: "G-FPY9CXM1LW"
-};
+  };
 
 firebase.initializeApp(firebaseConfig);
 
