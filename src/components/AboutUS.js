7484b6b02a1a4083d765b9c44f957a1a891a5f2a
Martin <martindeibe@gmail.com>
2023-03-10 16:02:22 +0100

    Cambios

--- src/components/AboutUS.js
+++ src/components/AboutUS.js
@@ -12,27 +12,34 @@ import AddShoppingCartSharpIcon from '@mui/icons-material/AddShoppingCartSharp';
 
 export const AboutUS = ({user}) => {
   return (
-    <div>
-      <Navbar user={user}/>
-
-      <div className="row">
-        <div className="col d-flex flex-column align-items-center mt-5">
-          <h3>Quienes somos?</h3>
-          <hr className="w-50" />
-          <p className="w-50">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <button className="button"><Link className="white" to={'/signup'}>Registrate</Link></button>
+    <div className="contenedor">
+      <Navbar id='div1' user={user}/>
+      <div id="div2"></div>
+      <div className="row mt-5 d-flex flex-row mb-5" id="div2-1">
+          <div className="col-md d-flex flex-column align-items-center justify-content-center">
+            <SportsBasketballIcon sx={{ fontSize: 60 }} />
+            <h2 className="mt-4">Nuestros jerseys</h2>
+            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+          </div>
+            <hr className="vertical"/>
+            <div className="col-md d-flex flex-column align-items-center justify-content-center">
+            <img className="nike" src={nikeLogo} alt='nike logo'></img>
+            <h2 className="mt-4">Nike</h2>
+            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+          </div>
+          <hr className="vertical"/>
+          <div className="col-md d-flex flex-column align-items-center justify-content-center">
+              <AddShoppingCartSharpIcon  sx={{ fontSize: 60 }} />
+              <h2 className="mt-4">Nuestro carro</h2>
+              <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+          </div>
         </div>
-      </div>
+      
 
-      <div className="row mt-3">
+
+
+
+      <div className="row mt-3" id='div3'>
         <div className="split-pane col-xs-12 col-md-6 left-side">
           <div>
             <div className="text-content">
@@ -54,28 +61,27 @@ export const AboutUS = ({user}) => {
         </div>
       </div>
 
-      <div className="row mt-5 d-flex flex-row mb-5">
-          <div className="col-md d-flex flex-column align-items-center justify-content-center">
-            <SportsBasketballIcon sx={{ fontSize: 60 }} />
-            <h2 className="mt-4">Nuestros jerseys</h2>
-            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-          </div>
-            <hr className="vertical"/>
-            <div className="col-md d-flex flex-column align-items-center justify-content-center">
-            <img className="nike" src={nikeLogo} alt='nike logo'></img>
-            <h2 className="mt-4">Nike</h2>
-            <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-          </div>
-          <hr className="vertical"/>
-          <div className="col-md d-flex flex-column align-items-center justify-content-center">
-              <AddShoppingCartSharpIcon  sx={{ fontSize: 60 }} />
-              <h2 className="mt-4">Nuestro carro</h2>
-              <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-          </div>
-
+      <div className="row" id="div5">
+        <div className="col d-flex flex-column justify-content-center align-items-center mt-5">
+          <h3>Quienes somos?</h3>
+          <hr className="w-50" />
+          <p className="w-50">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <button className="button"><Link className="white" to={'/signup'}>Registrate</Link></button>
+        </div>
       </div>
 
       <Footer />
+      <div id="div4"></div>
+
     </div>
+
   );
 };
